4676324abdf182503b0223ce7a3d11bbd78c99c0
Robert Rees <shudderfix@gmail.com>
2018-06-21 23:19:46 +0100

    Adds a navbar

--- app/repositories/collections.py
+++ app/repositories/collections.py
@@ -1,6 +1,31 @@
+import os
 
-def all_collections():
-    return []
+import dsnparse
+import pg8000
+from pypika import Table, Query
+
+DATABASE_URI = os.environ['DATABASE_URI']
+
+collections_table = Table('collections')
+r = dsnparse.parse(DATABASE_URI)
+
+def connect():
+    return pg8000.connect(r.username,
+        host=r.host,
+        password=r.password,
+        database=r.paths[0],
+        ssl=True)
+
+def all():
+    q = Query.from_(collections_table).select("*")
+
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(str(q))
+    results = [r for r in cursor.fetchall()]
+    cursor.close()
+    conn.close()
+    return results
 
 def create_collection(name):
     pass
