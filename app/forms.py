9878fc7612f5225a47af0ed909ccd0bb88c0f058
Robert Rees <shudderfix@gmail.com>
2018-06-23 22:04:55 +0100

    Creates some basic validation of the new collection submission

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from wtforms import Form, validators
+from wtforms.fields import StringField, BooleanField
+
+class NewCollection(Form):
+    name = StringField('Name', [validators.required()])
+    description = StringField('Description')
+    public = BooleanField('Public')
--- app/handlers/forms.py
+++ app/handlers/forms.py
@@ -1,4 +1,9 @@
-from flask import redirect, url_for
+from flask import redirect, request, url_for
+
+from app import forms
 
 def new_collection():
+    collection_form = forms.NewCollection(request.form)
+
+    assert collection_form.validate(), "Form data was not valid"
     return redirect(url_for('home'))
