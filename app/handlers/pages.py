2f2760b049fd94fe8cd2244a3a09e99f7a2c6ccc
Robert Rees <shudderfix@gmail.com>
2018-06-24 18:01:46 +0100

    Shows the collections on the home page

--- app/handlers/pages.py
+++ app/handlers/pages.py
@@ -1,7 +1,10 @@
 import flask
 
+from app.repositories import collections
+
 def front_page():
 	return flask.render_template('index.html')
 
 def home_page():
-	return flask.render_template('home.html')
+	return flask.render_template('home.html',
+		collections=collections.all(),)
