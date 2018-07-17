267cf9e0b77f3ab6ea88e083fc697da617c54c00
Robert Rees <shudderfix@gmail.com>
2018-07-17 23:26:06 +0100

    Saves the link to the links table

--- app/repositories/links.py
+++ app/repositories/links.py
@@ -5,8 +5,22 @@ from pypika import Table, Query
 
 from .connection import connect
 
-collections_table = Table('collections')
+links_table = Table('links')
 
 def add_link(collection_id, url, name=None, description=None):
 
-    return None
+	q = Query.into(links_table)\
+		.columns('collection_id', 'url', 'name', 'description')\
+		.insert(collection_id, url, name, description)
+
+	conn = connect()
+	cursor = conn.cursor()
+	cursor.execute(str(q) + 'RETURNING id')
+	link_id = cursor.fetchone()
+
+	cursor.close()
+
+	conn.commit()
+	conn.close()
+
+	return link_id
