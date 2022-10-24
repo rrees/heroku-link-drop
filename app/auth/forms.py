32dc6421c9381976820a70461f7bf0e6474d9488
Robert Rees <shudderfix@gmail.com>
2022-10-24 17:28:55 +0100

    Updates dependencies and WTForms syntax

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,5 +2,5 @@ from wtforms import Form, validators
 from wtforms import fields
 
 class Login(Form):
-    email = fields.StringField('Username', [validators.required()])
-    password = fields.PasswordField('Password', [validators.required()])
+    email = fields.StringField('Username', [validators.DataRequired()])
+    password = fields.PasswordField('Password', [validators.DataRequired()])
--- app/forms.py
+++ app/forms.py
@@ -2,32 +2,32 @@ from wtforms import Form, validators, widgets
 from wtforms import fields
 
 class NewCollection(Form):
-    name = fields.StringField('Name', [validators.required()])
+    name = fields.StringField('Name', [validators.DataRequired()])
     description = fields.StringField('Description')
     public = fields.BooleanField('Public', default=False)
 
 class NewLink(Form):
-    collection_id = fields.HiddenField('Collection ID', [validators.required()])
-    url = fields.StringField('URL', [validators.required()])
+    collection_id = fields.HiddenField('Collection ID', [validators.DataRequired()])
+    url = fields.StringField('URL', [validators.DataRequired()])
     name = fields.StringField('Name')
     description = fields.StringField('Description')
 
 class EditLink(Form):
-    url = fields.StringField('URL', [validators.required()])
+    url = fields.StringField('URL', [validators.DataRequired()])
     name = fields.StringField('Name')
     description = fields.StringField('Description')
 
 class DeleteItem(Form):
-    item_id = fields.HiddenField('Item Id', [validators.required()])
+    item_id = fields.HiddenField('Item Id', [validators.DataRequired()])
 
 class CollectionChange(Form):
-	collection_id = fields.HiddenField('Collection ID', [validators.required()])
+	collection_id = fields.HiddenField('Collection ID', [validators.DataRequired()])
 
 class CollectionEdit(Form):
-	name = fields.StringField('Name', [validators.required()])
+	name = fields.StringField('Name', [validators.DataRequired()])
 	description = fields.StringField('Description')
 
 class CollectionDelete(Form):
     item_id = fields.IntegerField('Collection ID',
         widget=widgets.HiddenInput(),
-        validators=[validators.required(), validators.NumberRange(min=0)])
+        validators=[validators.DataRequired(), validators.NumberRange(min=0)])
